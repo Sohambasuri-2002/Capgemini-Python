c9fec1c6ed58da23e7e6b7a1cb5c37c9472ea124
Sohambasuri-2002 <sohambasuri2015@gmail.com>
2025-05-18 11:58:11 +0530

    added files

--- practice.py
+++ practice.py
@@ -1,117 +1,106 @@
-# a,b = (1,2)
-# c=a+b
-# # print(c)
-# x,y,z = (1,2,3)
-# f=x+y+z
-# # print(f)
-# # print(type(x))
-# flo = 4.6
-# # print(type(flo))
-# x3 = True
-# # print(type(x3))
-# y = 10
-# print(str(y)," dollars")
-# print('Press "Enter"')
-# print('Red '+'Car')
-# print(3,5)
-# a = "Hello World"
-# print(a[1])
-# print(5*3)
-# print(5**3)
-# y = 125
-# print(y==125)
-# add = 2+1.5+5+\
-# 3
-# print(add)
-# print("Friday"[2])
-# def five(x) :
-#     x = 5
-#     return x
-# f = five(3)
-# print(f)
-# print(100>10)
-# print(6 is not 6)
-# if (3 == 15/5) :
-#     print("Yes")
+#1 SUM OF N NATURAL NUMBERS
+# num = int(input('Enter a number: '))
+# res = num*(num+1)/2
+# print(res)
+# sum = 0
+# for i in range(0,num+1):
+#     sum += i
+# print(sum)
+
+#2 SUM OF SQAURES OF N NATURAL NUMBERS
+# num = int(input('Enter a number: '))
+# res = 0
+# for i in range(1,num+1):
+#     res =  res + pow(i,2)
+# print(res)
+
+#3 EVEN NUMBERS 
+# num = int(input('Enter a range: '))
+# for i in range(0,num+1):
+#     if (i % 2 == 0):
+#         print(i,end=' ')
+
+#4 SUM OF DIGITS
+# num = int(input('Enter a range: '))
+# sum = 0
+# while(num != 0):
+#     rem = num % 10
+#     sum += rem
+#     num /= 10
+# print(int(sum))
+
+#S5 SUM OF SQUARES
+# num = int(input('Enter a range: '))
+# sum = 0
+# while(num != 0):
+#     rem = num % 10
+#     sum = sum + pow(rem,3)
+#     num = num // 10
+# print(sum)
+
+#6 ARMSTRONG NUMBERS 
+# num = int(input('Enter a range: '))
+# sum = 0
+# for i in range(1,num+1):
+#     rem = num%10
+#     sum *= pow(rem,3)
+#     num //= 10
+# print(sum)
+
+#7 REVERSE A NUMBER
+# num = int(input('Enter a range: '))
+# rev = 0
+# while(num != 0):
+#     rev = (rev*10) + (num%10)
+#     num //= 10
+# print(rev) 
+
+#8 PALINDROME NUMBERS 
+# num = int(input('Enter a range: '))
+# rev = 0
+# t = num
+# while(num != 0):
+#     rev = (rev*10) + (num%10)
+#     num //= 10
+# if (rev == t):
+#     print('Palindrome number')
 # else:
-#     print("No")
-# def compare_to_five(x):
-#     if (x>5):
-#         print("Greater")
-#     elif (x<1):
-#         print("Less than Zero")
-#     elif (x<5):
-#         print("Less than 5")
-#     else:
-#         print("Equal")
-# compare_to_five(-1)
-# def simple():
-#     print("Simple statement")
-# simple()
-# def plus_ten(x):
-#     result = x+10
-#     return result
-# a = plus_ten(10)
-# print(a)
-# def add_more(m):
-#     if (m >= 100):
-#         m = m+10
-#         return m
-#     else:
-#         return "Save more"
-# file = add_more(50)
-# print(file)
-# def substract(a,b,c):
-#     res = a-b*c
-#     print(a,b,c)
-#     return res
-# ans = substract(10,3,2)
-# print(ans)
-# print(int(5.00))
-# print(int(3.78))
-# print(max(10,3,4))
-# print(str(500))
-# z = -20
-# print(abs(z))
-# list_1 = [1,2,3,4]
-# print(sum(list_1))
-# print(round(3.5555,2))
-# print(pow(2,10))
-# print(len('maths'))
-# parti = ['john','leila','gregory','cate','nico']
-# print(parti)
-# print(parti[1])
-# print(parti[-1])
-# print(parti[-2])
-# parti[2] = 'Neha'
-# print(parti)
-# del(parti[2])
-# print(parti)
-# print(parti[2])
-# parti.append('Dwayne')
-# print(parti)
-# count = len(parti)
-# print(count)
-# print(len('Dolphin'))
-parti = ['A','B','C','D','E','F']
-# print(parti)
-# print(parti[1:3])
-# print(parti[:2])
-# print(parti[4:])
-# print(parti[-2:])
-# print(parti.index('D'))
-# numbers = [5,3,8,9,1]
-# numbers.sort()
-# print(numbers)
-x = (40,41,42)
-y = (50,51,52)
-# print(x)
-# a,b,c = 1,4,6
-# print(b)
-# print(x[0])
-# list_1 = [x,y]
-# print(list_1)
-(age, years) = "30,17".split(',')
-print(age)
-print(years)
+#     print('Not palindrome number')
+
+# 9 FIBONACCI NUMBERS
+# num = int(input('Enter a range: '))
+# x = 0
+# y = 1
+# print(x,end=' ')
+# print(y,end=' ')
+# for i in range(1,num+1):
+#     z = x+y
+#     print(z,end=' ')
+#     x=y
+#     y=z
+
+#10 MAX AND MIN NUMBERS
+# num = [4,8,3,7]
+# max = num[0]
+# for i in range(1,len(num)):
+#     if (num[i] > max):
+#         max = num[i]
+# print(max)
+
+#11 SECOND MAX
+# num = [4,8,3,7]
+# max = num[0]
+# for i in range(1,len(num)):
+#     if (num[i] > max):
+#         max = num[i]
+# print(max)
+# num.remove(max)
+# print(num)
+# max = num[0]
+# for i in range(1,len(num)):
+#     if (num[i] > max):
+#         max = num[i]
+# print('second max number is ',max)
+
+
 
